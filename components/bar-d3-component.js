f1525fa733454b5bb08750fa563d46ef71020e7c
Matt Wong <MattWong@Matts-MacBook-Pro-2.local>
2021-05-03 11:38:02 -0400

    Updated barchart to be more complex for food quantity

--- components/bar-d3-component.js
+++ components/bar-d3-component.js
@@ -3,16 +3,18 @@ const D3Component = require('idyll-d3-component');
 const d3 = require('d3');
 
 const globalWidth = 600;
+const globalHeight = 500;
 
 class BarD3Component extends D3Component {
   render;
 
   initialize(node, props) {
-    node.style.height = "500px";
+    console.log(props.data);
+    node.style.height = globalHeight+50;
 
-    var margin = {top: 30, right: 30, bottom: 70, left: 60};
+    var margin = {top: 30, right: 30, bottom: 90, left: 60};
     var width = globalWidth - margin.left - margin.right;
-    var height = 500 - margin.top - margin.bottom;
+    var height = globalHeight - margin.top - margin.bottom;
 
     const svg = (this.svg = d3.select(node)
     .append('svg')
@@ -28,20 +30,24 @@ class BarD3Component extends D3Component {
     .padding(0.2);
     var xAxis = svg.append("g")
     .attr("transform", "translate(0," + height + ")");
-
+    
     // Initialize the Y axis
     var y = d3.scaleLinear()
     .range([ height, 0]);
     var yAxis = svg.append("g")
     .attr("class", "myYaxis");
 
-    console.log(props.data);
-    var data = props.data;
-
-    this.render = (xAttr, yAttr) => {
+    this.render = (xAttr, yAttr, unit) => {
+      var data = this.updateData(props.data, unit);
       // X axis
-      x.domain(data.map(function(d) { return d.Timestamp; }))
+      x.domain(data.map(function(d) { return d.food; }))
       xAxis.transition().duration(1000).call(d3.axisBottom(x))
+      .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-45)");
+
 
       // Add Y axis
       y.domain([0, d3.max(data, function(d) { return +d[yAttr] }) ]);
@@ -64,12 +70,70 @@ class BarD3Component extends D3Component {
           .attr("fill", "#69b3a2")
     };
 
-    this.render(props.xAttr, props.yAttr);
+    this.render(props.xAttr, props.yAttr, props.unit);
     
   }
 
   update(props, oldProps) {
-    this.render(props.xAttr, props.yAttr);
+    this.render(props.xAttr, props.yAttr, props.unit);
+  }
+
+  // Unit type can either be 'lbs' or 'kgs'
+  updateData(data, unitType) {
+    const mapped_foods = [
+      "Beef / buffalo",
+      "Lamb / goat",
+      "Pork",
+      "Poultry (chicken, turkey, or any other bird meats)", 
+      "Fish (finfish)", 
+      "Crustaceans (e.g., shrimp, prawns, crabs, lobster)", 
+      "Mollusks (e.g., clams, oysters, squid, octopus)", 
+      "Butter",
+      "Cheese",
+      "Ice cream", 
+      "Cream", 
+      "Milk (cow's milk)",
+      "Yogurt", 
+      "Eggs",
+      "Potatoes",
+      "Cassava / Other roots", 
+      "Soybean oil", 
+      "Palm oil", 
+      "Sunflower oil",
+      "Rapeseed / canola oil", 
+      "Olive oil", 
+      "Beer", 
+      "Wine", 
+      "Coffee (Ground or whole bean)", 
+      "Sugar" 
+    ];
+    var final = [];
+    mapped_foods.forEach((food) => {
+      final.push({ food, count: 0 });
+    });
+
+    final.forEach((foodElement) => {
+      data.forEach((element) => {
+        const units = element["What unit of weight do you prefer to answer with? This will be the unit of weight corresponding to the amount of food you purchase per week."];
+        var convertedCount = element[foodElement["food"]];
+        if (units === "Pounds (lbs)") {
+          // Units are in lbs but we want kgs
+          if (unitType === "kgs") {
+            convertedCount = element[foodElement["food"]] * 0.4536
+          }
+        } else {
+          // Units are in kgs but we want lbs
+          if (unitType === "lbs") {
+            convertedCount = element[foodElement["food"]] * 2.2046
+          } 
+        }
+
+        foodElement["count"] += convertedCount;
+      });
+      foodElement["food"] = foodElement["food"].replace(/\(.*\)/, '');
+    });
+    final.sort((a,b) => (a.count > b.count) ? -1 : ((b.count > a.count) ? 1 : 0))
+    return final;
   }
     
 }
